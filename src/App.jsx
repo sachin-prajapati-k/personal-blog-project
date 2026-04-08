68568d0349010bb6d8696440a3ac83d3ab03d710
sagar <sdev77577@gmail.com>
2026-04-08 19:44:12 +0530

    adf

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import authService from "./appwrite/services/auth";
 import { useDispatch } from "react-redux";
 import { login, logout } from "./store/authSlice";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -23,8 +26,14 @@ function App() {
   return (
     <>
       {!loading ? (
-        <div className="min-h-screen w-full flex justify-center items-center">
-          loading...
+        <div className="min-h-screen w-full flex flex-wrap content-between bg-gray-400">
+          <div className="w-full block">
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+            <Footer />
+          </div>
         </div>
       ) : (
         <div></div>
