b9d849ca0ecfb239919b393d157a531b4de89915
sagar <sdev77577@gmail.com>
2026-04-08 19:40:58 +0530

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import authService from "./appwrite/services/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  console.log(import.meta.env.VITE_REACT_API_KEY);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (!userData) {
+          dispatch(logout());
+        } else {
+          dispatch(login(userData));
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return <>hello</>;
+  return (
+    <>
+      {!loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          loading...
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
 }
 
 export default App;
